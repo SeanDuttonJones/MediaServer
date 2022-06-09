085809ee06510e655cea14ee7e580f7122581fa2
seanduttonjones <sean4424@me.com>
2022-06-09 15:35:52 -0700

    Reverted Project Structure Changes

--- backend/src/main/java/com/duttonjones/MediaServer/IngestEngine/exceptions/InvalidParamterException.java
+++
@@ -1,8 +0,0 @@
-package com.duttonjones.MediaServer.IngestEngine.exceptions;
-
-public class InvalidParamterException extends RuntimeException {
-
-    public InvalidParamterException(String errorMessage) {
-        super(errorMessage);
-    }
-}
